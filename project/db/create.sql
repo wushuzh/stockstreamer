4da6333b83ca2e625b474f533f4160fa9cdcd232
wushuzh <zhangjiaqiang@gmail.com>
2017-11-04 14:48:40 +0800

    add postgres Dockerfile

---
+++ project/db/create.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE stocks;
+-- stock_price: contains records of stock prices with timestamps
+CREATE TABLE stock_prices (
+    stock_name varchar(6),
+    price decimal,
+    time timestamp);
+
+-- stock_image_urls: contains a URL where the company log for each stock may be found
+CREATE TABLE stock_image_urls (
+    stock_name varchar(6),
+    image_url varchar(1024));
+
+-- stock_highlow: conntains the 52-week high and low values for the stock
+CREATE TABLE stock_highlow(
+    stock_name varchar(6),
+    high_val52wk decimal,
+    low_val52wk decimal);
